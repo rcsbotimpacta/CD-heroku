2a87770b857d1719cb96d7c0ca659d6c1327ae37
rcsbotimpacta <89108486+rcsbotimpacta@users.noreply.github.com>
2021-09-17 20:27:26 -0300

    Update web.py

--- web.py
+++ web.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Richard Camargo- live 6 pipeline"
+    return "Richard Camargo- live 6 pipveline"
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
